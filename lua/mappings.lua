f2b6d55adefaa4aa083a2b904e49718a5839ac47
valmnt <valentin@stixe.org>
2025-05-21 23:43:25 +0200

    feat: accept codeium suggestions even if completion is showed

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,7 @@ map("n", "<leader>x", function()
 end, { desc = "Format current buffer with conform.nvim" })
 
 map("n", "<leader>lg", "<cmd>LazyGit<cr>")
+
+map("i", "<C-y>", function()
+    return vim.fn["codeium#Accept"]()
+end, { expr = true, silent = true, desc = "Accept Codeium suggestion" })
